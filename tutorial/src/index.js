18a4019bc472ea01e869fd562c22a59a42ae6a64
Jocelyn <jocelynboyd@pursuit.org>
2020-10-15 22:01:15 -0400

    Completes React Basics modules

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -22,47 +22,23 @@ import ReactDom from "react-dom";
 // Nested Components, React Tools
 
 //CSS 
-import './index.css' //-> this means the file is in the same folder -> relative path
-
-// setup vars
-const firstBook = {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth'
-};
-
-const secondBook = {
-  img: 'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
-  title: 'Our Class is a Family',
-  author: 'Shannon Olsen and Sandie Sonke'
-};
+import './index.css'; //-> this means the file is in the same folder -> relative path
+import {books} from './books'
+import SpecificBook from './Book'
+import {greeting} from './testing/testing'
 
 function BookList() {
+  console.log(greeting);
   return (
-  <section className='booklist'>
-    <Book
-      img={firstBook.img}
-      title={firstBook.title}
-      author={firstBook.author}
-      />
-    <Book
-      img={secondBook.img}
-      title={secondBook.title}
-      author={secondBook.author}
-      />
-  </section>
+    <section className='booklist'>
+      {books.map((book) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
+      })}
+    </section>
   );
  }
 
- // Book is a component and a function
-const Book = (props) => {
-  return ( <article className='book'>
-    <img src={props.img} alt=""/>
-    <h1>{props.title}</h1>
-    <h4>{props.author}</h4>    
-  </article>
-  );
-};
+
 
 // inline CSS is stronger than the CSS in index.css file and will override the properties -> JSX CSS, need curly braces tp access JSV 
 // const Author = () => <h4 style={{color:'#617d98', fontSize: '0.7frem', marginTop:'0.25rem'}}>Amelia Hepwork</h4>
